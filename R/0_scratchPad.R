ff1b1df319853cca034d3c291878138d521ba4e0
DAChristina <d.christina23@imperial.ac.uk>
2025-04-25 13:26:22 +0700

    update report

--- R/0_scratchPad.R
+++ R/0_scratchPad.R
@@ -14,99 +14,39 @@ ne_55_aveYear <- ne_55 %>%
   mutate(date = ymd(paste0(year, "-01-01"))) %>% 
   glimpse()
 
+################################################################################
+# report 1
 weekly_data <- ne_55_aveYear %>%
-  rowwise() %>%
-  mutate(
-    date_seq = list(seq(date, date + years(1) - days(1), by = "week")),
-    Ne = Ne / length(date_seq),
-    Ne_lb = Ne_lb / length(date_seq),
-    Ne_ub = Ne_ub / length(date_seq)
-  ) %>%
-  unnest(date_seq) %>%
-  # select(date = date_seq, value = value_per_week) %>%
-  mutate(type = "Weekly") %>% 
-  glimpse()
-
-
-
-# ne_55_distributed <- ne_55 %>% 
-#   dplyr::mutate(date = as.Date(date)) %>%
-#   dplyr::rowwise() %>%
-#   dplyr::mutate(
-#     days = list(seq.Date(date, date + years(1) - days(1), by = "day")),
-#     day_Ne = Ne / length(days),
-#     day_Ne_lb = Ne_lb / length(days),
-#     day_Ne_ub = Ne_ub / length(days)
-#   ) %>%
-#   tidyr::unnest(c(days)) %>%
-#   dplyr::rename(date_days = days) %>%
-#   glimpse()
-
-# test distribute weekly
-ne_55_distributed <- ne_55 %>% 
-  dplyr::mutate(date = as.Date(date)) %>%
   dplyr::rowwise() %>%
   dplyr::mutate(
-    weeks = list(seq.Date(date, date + years(1) - days(1), by = "week")),
-    day_Ne = Ne / length(weeks),
-    day_Ne_lb = Ne_lb / length(weeks),
-    day_Ne_ub = Ne_ub / length(weeks)
+    # weekly seq
+    weekly_seq = list(seq(date, date + years(1) - days(1), by = "week")),
+    Ne = Ne / length(weekly_seq),
+    Ne_lb = Ne_lb / length(weekly_seq),
+    Ne_ub = Ne_ub / length(weekly_seq)
   ) %>%
-  # tidyr::unnest(c(weeks)) %>%
-  dplyr::rename(date_weeks = weeks) %>% 
-  distinct(date_weeks, .keep_all = T) %>% 
-  glimpse()
-
-ggplot(ne_55_distributed, aes(x = date, y = day_Ne)) +
-  geom_line() +
-  geom_vline(xintercept = as.Date("2017-07-01"), color = "steelblue", linetype = "dashed") +
-  # geom_label(aes(x = as.Date("2017-07-01"), y = 40, label = "Jul-17"),
-  #            fill = "white", color = "black") +
-  # geom_point(data = test, aes(x = yearWeek, y = day_Ne), color = "steelblue", size = 1) +
-  geom_ribbon(data = ne_55_distributed,
-              aes(x = date, ymin = day_Ne_lb, ymax = day_Ne_ub),
-              inherit.aes = FALSE,
-              fill = "steelblue", alpha = 0.3
-  ) +
-  scale_x_date(limits = c(min(as.Date(gen$collection_date)), max(as.Date(dat_c$week_date))),
-               date_breaks = "1 year",
-               date_labels = "%Y") +
-  theme_bw()
-
-
-# test weekly, monthly & annually
-# ne_55_monthly <- ne_55_distributed %>%
-#   dplyr::group_by(yearMonth) %>%
-#   dplyr::summarise(day_Ne = sum(day_Ne), .groups = "drop") %>% 
-#   glimpse()
-# 
-ne_55_weekly <- weekly_data %>%
+  tidyr::unnest(weekly_seq) %>%
   dplyr::mutate(
-    iso_week = paste0(year(date), "-W", sprintf("%02d", week(date)), "-1"),
-    yearWeek =ISOweek::ISOweek2date(iso_week)
-  ) %>%
-  # dplyr::group_by(yearWeek) %>%
-  # dplyr::summarise(week_Ne = sum(day_Ne), .groups = "drop") %>%
+    iso_week = paste0(year(weekly_seq), "-W", sprintf("%02d", week(weekly_seq)), "-1"),
+    yearWeek = ISOweek::ISOweek2date(iso_week)
+  ) %>% 
+  ungroup() %>%
   glimpse()
+plot(weekly_data$yearWeek, weekly_data$Ne)
 
 # test combine interpolated data
 test <- allAges_weekly %>% 
   select(-contains("Ne")) %>% 
   dplyr::left_join(
     # ne_55_weekly
-    ne_55_weekly %>%
-      dplyr::mutate(
-        iso_week = paste0(year(date), "-W", sprintf("%02d", week(date)), "-1"),
-        yearWeek =ISOweek::ISOweek2date(iso_week)
-      )
+    weekly_data %>%
+      dplyr::select(yearWeek, contains("Ne"))
     ,
     by = c("yearWeek")
   ) %>% 
   glimpse()
 
-
-
-ggplot(test, aes(x = yearWeek, y = count, color = type.x)) +
+ggplot(test, aes(x = yearWeek, y = count, color = type)) +
   geom_line() +
   geom_vline(xintercept = as.Date("2017-07-01"), color = "steelblue", linetype = "dashed") +
   # geom_label(aes(x = as.Date("2017-07-01"), y = 40, label = "Jul-17"),
@@ -128,57 +68,8 @@ ggplot(test, aes(x = yearWeek, y = count, color = type.x)) +
         legend.background = element_rect(fill = "transparent", color = "transparent"))
 
 
-# report 1
-
-
-
-
-
-
+################################################################################
 # report 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 new_dates <- seq(from = as.Date(min(ne_55$date)), to = as.Date(max(ne_55$date)), by = "day") %>% 
   glimpse()
 
@@ -217,17 +108,36 @@ interpolated_df <- data.frame(
   ) %>% 
   glimpse()
 
-ggplot() +
+p1 <- ggplot() +
   geom_point(data = interpolated_df, aes(x = date, y = itr_Ne), color = "steelblue", size = 1) +
   geom_ribbon(data = interpolated_df,
               aes(x = date, ymin = itr_Ne_lo, ymax = itr_Ne_up),
               inherit.aes = FALSE,
               fill = "steelblue", alpha = 0.3
   ) +
+  annotate(
+    "rect",
+    xmin = min(as.Date(gen$collection_date)), xmax = as.Date("2021-03-20"),
+    ymin = 0, ymax = max(interpolated_df$itr_Ne)+50, na.rm = TRUE,
+    alpha = 0.1, fill = NA, colour = "skyblue"
+  ) +
   theme_bw()
 
 
+p2 <- ggplot() +
+  geom_point(data = interpolated_df, aes(x = date, y = itr_Ne), color = "steelblue", size = 1) +
+  geom_ribbon(data = interpolated_df,
+              aes(x = date, ymin = itr_Ne_lo, ymax = itr_Ne_up),
+              inherit.aes = FALSE,
+              fill = "steelblue", alpha = 0.3
+  ) +
+  scale_x_date(limits = c(min(as.Date(gen$collection_date)), as.Date("2021-03-20")),
+               date_breaks = "1 year",
+               date_labels = "%Y") +
+  theme_bw()
 
-
+cowplot::plot_grid(p1, p2,
+                   ncol = 1,
+                   labels = c("A", "B"))
 
 
