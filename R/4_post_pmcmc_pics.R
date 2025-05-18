1608df004817c9eb4d931472feb6d8519b0cebd8
DAChristina <d.christina23@imperial.ac.uk>
2025-05-18 15:41:24 +0700

    generate pics as fun

--- R/4_post_pmcmc_pics.R
+++ R/4_post_pmcmc_pics.R
@@ -1,204 +1,74 @@
 source("global/all_function_allAge.R")
 
-# load chains
-n_sts <- 600
-dir_name <- paste0("outputs/genomics/trial_", n_sts, "/")
-dir.create(paste0(dir_name, "/figs"), FALSE, TRUE)
-mcmc1 <- read.csv(paste0(dir_name, "mcmc1.csv"))
-mcmc2 <- read.csv(paste0(dir_name, "mcmc2.csv"))
-mcmc2_burnedin <- read.csv(paste0(dir_name, "mcmc2_burnedin.csv"))
-tune_pmcmc_result <- readRDS(paste0(dir_name, "tune_pmcmc_result.rds"))
-
-# mcmc1
-# fig <- pmcmc_trace(coda::as.mcmc(mcmc1))
-png(paste0(dir_name, "figs/mcmc1_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-pmcmc_trace(coda::as.mcmc(mcmc1))
-dev.off()
-
-# mcmc2
-# fig <- pmcmc_trace(coda::as.mcmc(mcmc2))
-png(paste0(dir_name, "figs/mcmc2_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-pmcmc_trace(coda::as.mcmc(mcmc2))
-dev.off()
-
-# mcmc2 burned in
-# fig <- pmcmc_trace(coda::as.mcmc(mcmc2))
-png(paste0(dir_name, "figs/mcmc2_burnedin_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-pmcmc_trace(coda::as.mcmc(mcmc2_burnedin))
-dev.off()
-
-# final parameters with CI
-tune_lpost_max <- which.max(tune_pmcmc_result$probabilities[, "log_posterior"])
-mcmc_lo_CI <- apply(tune_pmcmc_result$pars, 2, function(x) quantile(x, probs = 0.025))
-mcmc_hi_CI <- apply(tune_pmcmc_result$pars, 2, function(x) quantile(x, probs = 0.975))
-
-binds_tune_initial <- rbind(as.list(tune_pmcmc_result$pars[tune_lpost_max, ]), mcmc_lo_CI, mcmc_hi_CI)
-t_tune_initial <- t(binds_tune_initial)
-colnames(t_tune_initial) <- c("values", "low_CI", "high_CI")
-
-write.csv(t_tune_initial,
-          paste0(dir_name, "tune_initial_with_CI.csv"), row.names = T)
-
-# MCMC diagnostics
-# 1. Gelman-Rubin
-figs_gelman_init <- diag_init_gelman_rubin(tune_pmcmc_result)
-# fig <- diag_cov_mtx(figs_gelman_init)
-# fig <- diag_gelman_rubin(figs_gelman_init)
-
-png(paste0(dir_name, "figs/mcmc2_diag_gelmanRubin_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-diag_gelman_rubin(figs_gelman_init)
-dev.off()
-
-# 2. Autocorrelation
-png(paste0(dir_name, "figs/mcmc2_diag_auCorr_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-diag_aucorr(coda::as.mcmc(mcmc2))
-dev.off()
-
-# 3. ggpairs
-png(paste0(dir_name, "figs/mcmc2_diag_ggPairs_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-GGally::ggpairs(as.data.frame(tune_pmcmc_result$pars))
-dev.off()
-
-
-################################################################################
-# additional analysis for pmcmc samples
-pmcmc_samples <- readRDS(paste0(dir_name, "pmcmc_samples.rds"))
-data <- readRDS("inputs/pmcmc_data_week_allAge.rds")
-
-# gen_sir$new(pars = list(), time = 0, n_particles = 1L)$info()
-plot_states <- function(state, data) {
-  col <- grey(0.3, 0.1)
-  matplot(data$yearWeek, t(state["cases_55", , -1]),
-          type = "l", lty = 1, col = col,
-          xlab = "", ylab = "GPSC55 cases",
-          xlim = as.Date(c(min(data$yearWeek), max(data$yearWeek))),
-          ylim = c(0, max(data$count_WGS_GPSC55, na.rm = TRUE))
-          )
-  lines(data$yearWeek, data$count_WGS_GPSC55, col = 3, lty = 1, lwd = 2)
-
-  matplot(data$yearWeek, t(state["cases_12F", , -1]),
-          type = "l", lty = 1, col = col,
-          xlab = "", ylab = "12F cases",
-          xlim = as.Date(c(min(data$yearWeek), max(data$yearWeek))),
-          ylim = c(0, max(data$count_serotype, na.rm = TRUE))
-          )
-  lines(data$yearWeek, data$count_serotype, col = 2, lty = 1, lwd = 2)
-  lines(data$yearWeek, data$count_WGS_GPSC55 + data$count_WGS_non55,
-         col = 3, lty = 1, lwd = 2)
-
-  matplot(data$yearWeek, t(state["cases_non55", , -1]),
-          type = "l", lty = 1, col = col,
-          xlab = "", ylab = "non55 cases",
-          ylim = c(0, max(data$count_WGS_non55, na.rm = TRUE))
-          )
-  lines(data$yearWeek, data$count_WGS_non55, col = 2,lty = 1, lwd = 2)
-
-  matplot(data$yearWeek, t(state["n_AD_weekly", , -1]),
-          type = "l", lty = 1, col = col,
-          xlab = "", ylab = "Diseased cases (Infections)")
-
-  matplot(data$yearWeek, t(state["Ne", , -1]),
-          type = "l", lty = 1, col = col,
-          xlab = "", ylab = "Ne")
-  lines(data$yearWeek, data$Ne, col = 2, lty = 1, lwd = 2)
-
-  # matplot(data$yearWeek, xlab = "", t(state["S_tot", , -1]),
-  #         type = "l", lty = 1, col = 2, ylab = "%", ylim = c(0, 6e7), yaxt = "n")
-  # axis(side = 2, at = seq(0, 6e7, length.out = 5),
-  #      labels = seq(0, 100, length.out = 5))
-
-  # matlines(data$yearWeek, t(state["I_tot", , -1]), lty = 1, col = 3)
-  # matlines(data$yearWeek, t(state["R_tot", , -1]), lty = 1, col = 4)
-  # legend("left", bty = "n", fill = 2:4, legend = c("S", "I", "R"))
-  # 
-  # matplot(data$yearWeek, xlab = "", t(state["D_tot", , -1]),
-  #         type = "l", lty = 1, col = 3, ylab = "carriers")
+# generate post-mcmc picts
+post_pmcmc_pics <- function(n_sts){
+  dir_name <- paste0("outputs/genomics/trial_", n_sts, "/")
+  dir.create(paste0(dir_name, "/figs"), FALSE, TRUE)
+  mcmc1 <- read.csv(paste0(dir_name, "mcmc1.csv"))
+  mcmc2 <- read.csv(paste0(dir_name, "mcmc2.csv"))
+  mcmc2_burnedin <- read.csv(paste0(dir_name, "mcmc2_burnedin.csv"))
+  tune_pmcmc_result <- readRDS(paste0(dir_name, "tune_pmcmc_result.rds"))
   
+  # mcmc1
+  # fig <- pmcmc_trace(coda::as.mcmc(mcmc1))
+  png(paste0(dir_name, "figs/mcmc1_%02d.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  pmcmc_trace(coda::as.mcmc(mcmc1))
+  dev.off()
   
+  # mcmc2
+  # fig <- pmcmc_trace(coda::as.mcmc(mcmc2))
+  png(paste0(dir_name, "figs/mcmc2_%02d.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  pmcmc_trace(coda::as.mcmc(mcmc2))
+  dev.off()
+  
+  # mcmc2 burned in
+  # fig <- pmcmc_trace(coda::as.mcmc(mcmc2))
+  png(paste0(dir_name, "figs/mcmc2_burnedin_%02d.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  pmcmc_trace(coda::as.mcmc(mcmc2_burnedin))
+  dev.off()
+  
+  # final parameters with CI
+  tune_lpost_max <- which.max(tune_pmcmc_result$probabilities[, "log_posterior"])
+  mcmc_lo_CI <- apply(tune_pmcmc_result$pars, 2, function(x) quantile(x, probs = 0.025))
+  mcmc_hi_CI <- apply(tune_pmcmc_result$pars, 2, function(x) quantile(x, probs = 0.975))
+  
+  binds_tune_initial <- rbind(as.list(tune_pmcmc_result$pars[tune_lpost_max, ]), mcmc_lo_CI, mcmc_hi_CI)
+  t_tune_initial <- t(binds_tune_initial)
+  colnames(t_tune_initial) <- c("values", "low_CI", "high_CI")
+  
+  write.csv(t_tune_initial,
+            paste0(dir_name, "tune_initial_with_CI.csv"), row.names = T)
+  
+  # MCMC diagnostics
+  # 1. Gelman-Rubin
+  figs_gelman_init <- diag_init_gelman_rubin(tune_pmcmc_result)
+  # fig <- diag_cov_mtx(figs_gelman_init)
+  # fig <- diag_gelman_rubin(figs_gelman_init)
+  
+  png(paste0(dir_name, "figs/mcmc2_diag_gelmanRubin_%02d.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  diag_gelman_rubin(figs_gelman_init)
+  dev.off()
+  
+  # 2. Autocorrelation
+  png(paste0(dir_name, "figs/mcmc2_diag_auCorr_%02d.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  diag_aucorr(coda::as.mcmc(mcmc2))
+  dev.off()
+  
+  # 3. ggpairs
+  png(paste0(dir_name, "figs/mcmc2_diag_ggPairs_%02d.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  GGally::ggpairs(as.data.frame(tune_pmcmc_result$pars))
+  dev.off()
 }
 
+# a slight modification for college's HPC
+args <- commandArgs(trailingOnly = T)
+n_sts <- as.numeric(args[which(args == "--n_steps") + 1])
 
-
-
-
-
-
-
-
-
-
-
-
-
-# png("figs/traces.png", width = 17, height = 12, unit = "cm", res = 1200)
-# par(mfrow = c(3, 4), mar = c(3, 3, 1, 1), mgp = c(1.7, 0.7, 0), bty = "n")
-# plot_traces(pmcmc_results$pars, metrics = pmcmc_results$metrics)
-# plot_traces(pmcmc_results$probabilities)
-# dev.off()
-
-
-# png("figs/posteriors.png", width = 17, height = 8, unit = "cm", res = 1200)
-# par(mfrow = c(3, 3), mar = c(3, 3, 1, 1), mgp = c(1.7, 0.7, 0), bty = "n")
-# for (nm in names(initial_pars)) {
-#   hist(pmcmc_samples$pars[, nm], xlab = nm, main = "", freq = FALSE)
-#   if (nm %in% names(priors)) {
-#     curve(exp(priors[[nm]](x)), add = TRUE, col = 2)
-#   }
-# }
-# dev.off()
-
-
-# fixed pics
-png(paste0(dir_name, "figs/pmcmc_samples_fits.png"),
-    width = 24, height = 17, unit = "cm", res = 600)
-par(mfrow = c(3, 2), bty = "n", mar = c(3, 3, 1, 1), mgp = c(1.7, 0.7, 0))
-plot_states(pmcmc_samples$trajectories$state, data)
-dev.off()
-
-png(paste0(dir_name, "figs/pmcmc_samples_diag_ggPairs_%02d.png"),
-    width = 17, height = 17, unit = "cm", res = 600)
-par(mar = c(3, 3, 1, 1), mgp = c(1.7, 0.7, 0))
-GGally::ggpairs(as.data.frame(pmcmc_samples$pars))
-dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+post_pmcmc_pics(n_sts)
 
--- model/sir_stochastic_allAge_2post_pmcmc_picts.R
+++ model/sir_stochastic_allAge_2post_pmcmc_picts.R
@@ -5,139 +5,150 @@ library(odin.dust)
 # remotes::install_github("mrc-ide/odin.dust")
 source("global/all_function_allAge.R")
 
-# load chains
-n_sts <- 600
-dir_name <- paste0("outputs/genomics/trial_", n_sts, "/")
-dir.create(paste0(dir_name, "/figs"), FALSE, TRUE)
-# run 4_post_pmcmc_pics.R first
-results <- read.csv(paste0(dir_name, "tune_initial_with_CI.csv")) %>% 
-  glimpse()
 
-
-# gen_sir <- odin.dust::odin_dust("model/sir_basic_trial.R")
-gen_sir <- odin.dust::odin_dust("model/sir_stochastic_allAge.R")
-
-# Running the SIR model with dust
-pars <- list(N_ini = 6.7e7,
-             scaled_A_ini = results[1,2],
-             D_ini = 0,
-             R_ini = 0,
-             time_shift_1 = results[2,2],
-             time_shift_2 = results[3,2],
-             beta_0 = results[4,2],
-             beta_1 = results[5,2],
-             beta_2 = results[6,2],
-             scaled_wane = results[7,2],
-             # psi = (0.5),
-             hypo_sigma_2 = (1),
-             log_delta = results[8,2]
-             # alpha = results[9,2],
-             # gamma_annual = results[10,2],
-             # nu_annual = results[11,2]
-)
-
-time_points <- round(seq(0, by = (365/52), length.out = 52*3)) # per-week, 22 years
-# n_times <- length(time_points)
-n_times <- 13800 # roughly from 1987-2025 in days
-sir_model <- gen_sir$new(pars = pars,
-                         time = 1,
-                         n_particles = 1L,
-                         n_threads = 4L,
-                         seed = 1L)
-
-# compartment position check
-# sir_model$info()
-# sir_model$info()$index$n_AD_weekly
-# update_state is required "every single time" to run & produce matrix output (don't know why)
-# sir_model$update_state(pars = pars,
-#                        time = 0) # make sure time is 0
-
-# all_date <- incidence$day
-# all_date <- data.frame(col = integer(4745))
-# incidence <- read.csv("inputs/incidence_week_12F_allAge.csv") %>% 
-#   dplyr::mutate(day = week*7) 
-n_particles <- 1L
-model <- array(NA, dim = c(sir_model$info()$len, n_particles, n_times))
-
-# R0 estimation
-R0 <- pars$beta_0/pars$sigma_2
-R0
-
-for (t in seq_len(n_times)) {
-  model[ , , t] <- sir_model$run(t)
-}
-# time <- x[1, 1, ] # because in the position of [1, 1, ] is time
-# x <- x[-1, , ] # compile all matrix into 1 huge df, delete time (position [-1, , ])
-
-sir_data <- readRDS("inputs/pmcmc_data_week_allAge.rds") %>% 
-  dplyr::transmute(
-    replicate = 1,
-    steps = seq_along(yearWeek),
-    value = count_WGS_GPSC55,
-    compartment = "data_count_WGS_GPSC55"
-  ) %>% 
-  glimpse()
-
-all_dates <- readRDS("inputs/pmcmc_data_week_allAge.rds") %>% 
-  dplyr::transmute(
-    yearWeek = as.Date(yearWeek),
-    steps = seq_along(yearWeek)
-  ) %>% 
-  glimpse()
-
-# n_AD_weekly have already in weeks
-incidence_modelled <- 
-  reshape2::melt(model) %>% 
-  dplyr::rename(index = Var1,     # Var1 = dimension that stored SADR values
-                replicate = Var2, # Var2 = particles
-                steps = Var3       # Var3 = steps are in days, but n_AD_weekly is aggregated in weeeks
-  ) %>% 
-  # dplyr::filter(index < 5) %>% 
-  dplyr::mutate(compartment = 
-                  dplyr::case_when(index == 1 ~ "Time",
-                                   index == 2 ~ "A",
-                                   index == 3 ~ "D",
-                                   index == 4 ~ "S",
-                                   index == 5 ~ "R",
-                                   index == 6 ~ "n_AD_weekly",
-                                   index == 7 ~ "Ne",
-                                   index == 8 ~ "cases_55",
-                                   index == 9 ~ "cases_non55",
-                                   index == 10 ~ "cases_12F"
-                  )) %>% 
-  dplyr::rename(value = value) %>% 
-  dplyr::select(-index) %>% 
-  dplyr::bind_rows(sir_data) %>% 
-  dplyr::full_join(
-    all_dates
-    ,
-    by = "steps"
-  ) %>% 
-  glimpse()
-
-ggplot(incidence_modelled %>% 
-         dplyr::filter(# grepl("cases|D|data", compartment),
-           compartment %in% c("D", "count_WGS_GPSC55"),
-           # compartment != "Time"
-                      )
+model_vs_data <- function(n_sts){
+  dir_name <- paste0("outputs/genomics/trial_", n_sts, "/")
+  dir.create(paste0(dir_name, "/figs"), FALSE, TRUE)
+  # run 4_post_pmcmc_pics.R first
+  results <- read.csv(paste0(dir_name, "tune_initial_with_CI.csv")) %>% 
+    glimpse()
+  
+  
+  # gen_sir <- odin.dust::odin_dust("model/sir_basic_trial.R")
+  gen_sir <- odin.dust::odin_dust("model/sir_stochastic_allAge.R")
+  
+  # Running the SIR model with dust
+  pars <- list(N_ini = 6.7e7,
+               scaled_A_ini = results[1,2],
+               D_ini = 0,
+               R_ini = 0,
+               time_shift_1 = results[2,2],
+               time_shift_2 = results[3,2],
+               beta_0 = results[4,2],
+               beta_1 = results[5,2],
+               beta_2 = results[6,2],
+               scaled_wane = results[7,2],
+               # psi = (0.5),
+               hypo_sigma_2 = (1),
+               log_delta = results[8,2]
+               # alpha = results[9,2],
+               # gamma_annual = results[10,2],
+               # nu_annual = results[11,2]
+  )
+  
+  time_points <- round(seq(0, by = (365/52), length.out = 52*3)) # per-week, 22 years
+  # n_times <- length(time_points)
+  n_times <- 13800 # roughly from 1987-2025 in days
+  sir_model <- gen_sir$new(pars = pars,
+                           time = 1,
+                           n_particles = 1L,
+                           n_threads = 4L,
+                           seed = 1L)
+  
+  # compartment position check
+  # sir_model$info()
+  # sir_model$info()$index$n_AD_weekly
+  # update_state is required "every single time" to run & produce matrix output (don't know why)
+  # sir_model$update_state(pars = pars,
+  #                        time = 0) # make sure time is 0
+  
+  # all_date <- incidence$day
+  # all_date <- data.frame(col = integer(4745))
+  # incidence <- read.csv("inputs/incidence_week_12F_allAge.csv") %>% 
+  #   dplyr::mutate(day = week*7) 
+  n_particles <- 1L
+  model <- array(NA, dim = c(sir_model$info()$len, n_particles, n_times))
+  
+  # R0 estimation
+  R0 <- pars$beta_0/pars$sigma_2
+  R0
+  
+  for (t in seq_len(n_times)) {
+    model[ , , t] <- sir_model$run(t)
+  }
+  # time <- x[1, 1, ] # because in the position of [1, 1, ] is time
+  # x <- x[-1, , ] # compile all matrix into 1 huge df, delete time (position [-1, , ])
+  
+  sir_data <- readRDS("inputs/pmcmc_data_week_allAge.rds") %>% 
+    dplyr::transmute(
+      replicate = 1,
+      steps = seq_along(yearWeek),
+      value = count_WGS_GPSC55,
+      compartment = "data_count_WGS_GPSC55"
+    ) %>% 
+    glimpse()
+  
+  all_dates <- readRDS("inputs/pmcmc_data_week_allAge.rds") %>% 
+    dplyr::transmute(
+      yearWeek = as.Date(yearWeek),
+      steps = seq_along(yearWeek)
+    ) %>% 
+    glimpse()
+  
+  # n_AD_weekly have already in weeks
+  incidence_modelled <- 
+    reshape2::melt(model) %>% 
+    dplyr::rename(index = Var1,     # Var1 = dimension that stored SADR values
+                  replicate = Var2, # Var2 = particles
+                  steps = Var3       # Var3 = steps are in days, but n_AD_weekly is aggregated in weeeks
+    ) %>% 
+    # dplyr::filter(index < 5) %>% 
+    dplyr::mutate(compartment = 
+                    dplyr::case_when(index == 1 ~ "Time",
+                                     index == 2 ~ "A",
+                                     index == 3 ~ "D",
+                                     index == 4 ~ "S",
+                                     index == 5 ~ "R",
+                                     index == 6 ~ "n_AD_weekly",
+                                     index == 7 ~ "Ne",
+                                     index == 8 ~ "cases_55",
+                                     index == 9 ~ "cases_non55",
+                                     index == 10 ~ "cases_12F"
+                    )) %>% 
+    dplyr::rename(value = value) %>% 
+    dplyr::select(-index) %>% 
+    dplyr::bind_rows(sir_data) %>% 
+    dplyr::full_join(
+      all_dates
+      ,
+      by = "steps"
+    ) %>% 
+    glimpse()
+  
+  png(paste0(dir_name, "figs/model_vs_data.png"),
+      width = 17, height = 17, unit = "cm", res = 600)
+  ggplot(incidence_modelled %>% 
+           dplyr::filter(grepl("AD|data", compartment),
+                         # compartment %in% c("D", "count_WGS_GPSC55"),
+                         # compartment != "Time"
+           )
          ,
-       aes(x = yearWeek, y = value,
-           group = interaction(compartment,replicate),
-           colour = compartment)) +
-  geom_line() +
-  geom_line() +
-  # scale_y_continuous(trans = "log1p") +
-  scale_y_continuous(limits = c(0, 50)) +
-  # scale_x_continuous(limits = c(0, 700)) +
-  scale_x_date(limits = c(as.Date(min(all_dates$yearWeek)), as.Date(max(all_dates$yearWeek)))) +
-  ggtitle("Cases (Aggregated by Week)") +
-  xlab("Time") +
-  ylab("Number of People") +
-  theme_bw() +
-  theme(legend.position = c(0.15, 0.85),
-        legend.title = element_blank(),
-        legend.key.size = unit(0.8, "lines"),
-        legend.text = element_text(size = 10),
-        legend.background = element_rect(fill = "transparent", color = "transparent"))
+         aes(x = yearWeek, y = value,
+             group = interaction(compartment,replicate),
+             colour = compartment)) +
+    geom_line() +
+    geom_line() +
+    # scale_y_continuous(trans = "log1p") +
+    scale_y_continuous(limits = c(0, 50)) +
+    # scale_x_continuous(limits = c(0, 700)) +
+    scale_x_date(limits = c(as.Date(min(all_dates$yearWeek)), as.Date(max(all_dates$yearWeek)))) +
+    ggtitle("Cases (Aggregated by Week)") +
+    xlab("Time") +
+    ylab("Number of People") +
+    theme_bw() +
+    theme(legend.position = c(0.15, 0.85),
+          legend.title = element_blank(),
+          legend.key.size = unit(0.8, "lines"),
+          legend.text = element_text(size = 10),
+          legend.background = element_rect(fill = "transparent", color = "transparent"))
+  dev.off()
+  
+}
+
+# a slight modification for college's HPC
+args <- commandArgs(trailingOnly = T)
+n_sts <- as.numeric(args[which(args == "--n_steps") + 1])
+
+model_vs_data(n_sts)
 
 
