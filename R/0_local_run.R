8eea300a5e547ece0b9e4ce2271d4ca5792afbcb
DAChristina <chrisdewiress@gmail.com>
2025-12-13 20:52:34 +0700

    test constraint time_shift

--- R/0_local_run.R
+++ R/0_local_run.R
@@ -6,6 +6,19 @@ source("R/4_post_pmcmc_pics.R")
 source("R/5_post_pmcmc_samples_pics.R")
 source("R/6_post_pmcmc_age_validation.R")
 
+# test vcv error
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 600,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 600,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 600,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 600,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 600,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+
+
 pmcmc_run_plus_tuning(n_pars = 10, n_sts = 600,
                       run1_stochastic = F, run2_stochastic = F, ncpus = 60)
 
@@ -23,4 +36,24 @@ post_pmcmc_pics(2000)
 model_vs_data(2000)
 post_particle_pics(2000)
 # age_validation(2000)
+
+
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 5000,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+
+post_pmcmc_pics(5000)
+model_vs_data(5000)
+post_particle_pics(5000)
+# age_validation(2000)
+
+
+pmcmc_run_plus_tuning(n_pars = 10, n_sts = 10000,
+                      run1_stochastic = F, run2_stochastic = F, ncpus = 60)
+
+post_pmcmc_pics(10000)
+model_vs_data(10000)
+post_particle_pics(10000)
+# age_validation(2000)
+
+
 
