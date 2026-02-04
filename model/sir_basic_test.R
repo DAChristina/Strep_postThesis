b656dd164d800dd060fc0088cd0dcf249413e5a5
DAChristina <chrisdewiress@gmail.com>
2026-02-05 00:39:45 +0700

    test ageing factor

--- model/sir_basic_test.R
+++ model/sir_basic_test.R
@@ -35,13 +35,17 @@ t_norm <- transmission/max(transmission)
 
 pars <- list(m = t_norm,
              N_ini = contact_2_demographic$demography$population,
-             log_I_ini = c(0.35, 0.3), # test c(0.65, 0.35),
+             log_E_ini = c(0.5, 0.5), # test c(0.65, 0.35),
+             I_ini = c(3,3),
+             log_delta1 = -5,
+             log_delta2 = -5,
+             hypo_sigma_1_day = 16,
              time_shift_1 = 0.1254,
              beta_0 = 0.5,
              beta_1 = 0.3
 )
 
-n_times <- 7500 # 500 for trial
+n_times <- 7500*24 # 500 for trial
 n_pars <- 1L
 sir_model <- gen_sir$new(pars = pars,
                          time = 1,
@@ -110,21 +114,39 @@ incidence_modelled <-
                   dplyr::case_when(index == 1 ~ "Time",
                                    index == 2 ~ "total N",
                                    index == 3 ~ "total S",
-                                   index == 4 ~ "total I",
-                                   index == 5 ~ "total R",
-                                   index == 6 ~ "n_SI1_weekly",
-                                   index == 7 ~ "n_SI2_weekly",
+                                   index == 4 ~ "total E",
                                    
-                                   index == 8 ~ "S <14",
-                                   index == 9 ~ "S 15+",
-                                   index == 10 ~ "model_D1",
-                                   index == 11 ~ "model_D2",
-                                   index == 12 ~ "R <14",
-                                   index == 13 ~ "R 15+"
+                                   index == 5 ~ "total I",
+                                   index == 6 ~ "total R",
+                                   index == 7 ~ "n_EI1_weekly",
+                                   index == 8 ~ "n_EI2_weekly",
+                                   index == 9 ~ "beta",
+                                   
+                                   index == 10 ~ "S <14",
+                                   index == 11 ~ "S 15+",
+                                   index == 12 ~ "E1",
+                                   index == 13 ~ "E2",
+                                   index == 14 ~ "model_D1",
+                                   index == 15 ~ "model_D2",
+                                   index == 16 ~ "R <14",
+                                   index == 17 ~ "R 15+",
+                                   index == 18 ~ "lambda1",
+                                   index == 19 ~ "lambda2"
+                                   
                                    
                   )) %>% 
   dplyr::select(-index) %>%
-  dplyr::mutate(weekly = ceiling(steps/7)) %>% 
+  # step --> time (day) --> week adjustment
+  dplyr::mutate(
+    # day = (steps - 1) %/% 24 + 1
+    day = (steps - 1) %/% 1 + 1
+  ) %>%
+  dplyr::group_by(replicate, day, compartment) %>%
+  summarise(
+    value = sum(value, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  dplyr::mutate(weekly = ceiling(day/7)) %>%
   dplyr::group_by(replicate, weekly, compartment) %>% 
   dplyr::summarise(value = sum(value, na.rm = T),
                    # date = max(date),
@@ -144,7 +166,8 @@ incidence_modelled <-
 
 p1 <- ggplot(incidence_modelled %>% 
                dplyr::filter(
-                 compartment %in% c("model_D1", "n_SI1_weekly",
+                 compartment %in% c("model_D1",
+                                    "n_EI1_weekly",
                                     "data_count_s1_1"),
                  compartment != "Time",
                )
@@ -156,6 +179,7 @@ p1 <- ggplot(incidence_modelled %>%
   geom_vline(aes(xintercept = as.Date("2010-04-01"),
                  colour = "PCV13 (April 2010)"),
              linetype = "dashed") +
+  # scale_y_continuous(limits = c(0, 40)) +
   scale_x_date(limits = c(as.Date(min(all_dates$yearWeek)), as.Date(max(all_dates$yearWeek))),
                date_breaks = "year",
                date_labels = "%Y") +
@@ -171,7 +195,8 @@ p1 <- ggplot(incidence_modelled %>%
 
 p2 <- ggplot(incidence_modelled %>% 
                dplyr::filter(
-                 compartment %in% c("model_D2", "n_SI2_weekly",
+                 compartment %in% c("model_D2",
+                                    "n_EI2_weekly",
                                     "data_count_s1_2"),
                  compartment != "Time",
                )
@@ -180,6 +205,7 @@ p2 <- ggplot(incidence_modelled %>%
                  group = interaction(compartment,replicate),
                  colour = compartment)) +
   geom_line() +
+  # scale_y_continuous(limits = c(0, 40)) +
   scale_x_date(limits = c(as.Date(min(all_dates$yearWeek)), as.Date(max(all_dates$yearWeek))),
                date_breaks = "year",
                date_labels = "%Y") +
@@ -199,3 +225,71 @@ p_combined <- cowplot::plot_grid(p1, p2,
 
 
 print(p_combined)
+
+# lambda & beta figs ###########################################################
+p1 <- ggplot(incidence_modelled %>%
+               dplyr::filter(
+                 compartment %in% c(
+                   # "lambda1",
+                   "beta"
+                 ),
+                 compartment != "Time",
+               )
+             ,
+             aes(x = yearWeek, y = value,
+                 group = interaction(compartment,replicate),
+                 colour = compartment)) +
+  geom_line() +
+  geom_vline(aes(xintercept = as.Date("2010-04-01"),
+                 colour = "PCV13 (April 2010)"),
+             linetype = "dashed") +
+  scale_x_date(limits = c(as.Date(min(all_dates$yearWeek)), as.Date("2018-03-27")),
+               date_breaks = "year",
+               date_labels = "%Y") +
+  # scale_y_continuous(limits = c(0.1225,0.1250)) +
+  ggtitle("Cases (Aggregated by Week) for age 0-14") +
+  xlab("Time") +
+  ylab("Number of People") +
+  theme_bw() +
+  theme(legend.position = c(0.85, 0.85),
+        legend.title = element_blank(),
+        legend.key.size = unit(0.8, "lines"),
+        legend.text = element_text(size = 10),
+        legend.background = element_rect(fill = "transparent", color = "transparent"))
+
+p2 <- ggplot(incidence_modelled %>%
+               dplyr::filter(
+                 compartment %in% c(
+                   # "lambda2",
+                   "beta"
+                 ),
+                 compartment != "Time",
+               )
+             ,
+             aes(x = yearWeek, y = value,
+                 group = interaction(compartment,replicate),
+                 colour = compartment)) +
+  geom_line() +
+  scale_x_date(limits = c(as.Date(min(all_dates$yearWeek)), as.Date("2018-03-27")),
+               date_breaks = "year",
+               date_labels = "%Y") +
+  # scale_y_continuous(limits = c(0,0.1)) +
+  ggtitle("Cases (Aggregated by Week) for age 15+") +
+  xlab("Time") +
+  ylab("Number of People") +
+  theme_bw() +
+  theme(legend.position = c(0.85, 0.85),
+        legend.title = element_blank(),
+        legend.key.size = unit(0.8, "lines"),
+        legend.text = element_text(size = 10),
+        legend.background = element_rect(fill = "transparent", color = "transparent"))
+
+p_combined <- cowplot::plot_grid(p1, p2,
+                                 nrow =2,
+                                 labels = c("A", "B"))
+
+
+print(p_combined)
+
+
+
--- model/sir_basic_trial.R
+++ model/sir_basic_trial.R
@@ -1,18 +1,25 @@
-freq <- user(24) # 24 steps per day; prev model is daily but aggregated to weekly
+freq <- user(1) # 1 step per day; prev model is daily but aggregated to weekly
 dt <- 1/freq
 initial(time) <- 0
 update(time) <- (step + 1) * dt
+
+# burn_in <- 
 
 # 1. PARAMETERS ################################################################
 time_shift_1 <- user(0, min = 0)
 beta_0 <- user(0, min = 0)
 beta_1 <- user(0, min = 0)
 theta <- 0.19 # proportion of vaccinated children in 0-14 age group
+
+hypo_sigma_1_day <- user() #15.75 # (95% CI 7.88-31.49) (Chaguza et al., 2021)
+sigma_1 <- 1/hypo_sigma_1_day # test sigma_1 (A -> R) later
 sigma_2 <- 1 # Assumed acute phase, 1 day
 
 mu_0[1] <- 1/((80.70-14)*365)
 mu_0[2] <- 1/(14*365)
 mu_1 <- 0 # disease-related death, no data available
+alpha[1] <- 1/(15*365) # ageing child -> adult
+alpha[2] <- 0
 pi <- 3.141593 # FIXED
 wane <- 0
 
@@ -20,64 +27,104 @@ wane <- 0
 N_age <- 2
 
 dim(N_ini) <- N_age
-dim(I_ini) <- N_age
-dim(log_I_ini) <- N_age
+dim(E_ini) <- N_age
+# dim(I_ini) <- N_age
+dim(log_E_ini) <- N_age
 # dim(S_ini) <- N_age
 
 dim(N) <- N_age
 dim(S) <- N_age
+dim(E) <- N_age
 dim(I) <- N_age
 dim(R) <- N_age
 
 dim(m) <- c(N_age, N_age)
 dim(foi_ij) <- c(N_age, N_age)
 dim(vacc_m) <- c(N_age, N_age)
+dim(delta) <- N_age
 dim(lambda) <- N_age
 dim(mu_0) <- N_age
+dim(alpha) <- N_age
+dim(born) <- N_age
 
 dim(p_Suscep) <- N_age
+dim(p_Exposd) <- N_age
 dim(p_Infec) <- N_age
 dim(p_Recov) <- N_age
 
+dim(p_SE) <- N_age
+dim(p_EI) <- N_age
+dim(p_ER) <- N_age
+dim(p_IR) <- N_age
+dim(p_Id) <- N_age
+dim(p_RS) <- N_age
+
+dim(p_Sage) <- N_age
+dim(p_Eage) <- N_age
+dim(p_Iage) <- N_age
+dim(p_Rage) <- N_age
+
 dim(n_Sborn) <- N_age
 dim(n_Suscep) <- N_age
-dim(n_SI) <- N_age
+dim(n_SE) <- N_age
 # dim(n_SR) <- N_age
+dim(n_Sage) <- N_age
 dim(n_Sdead) <- N_age
+dim(n_Exposd) <- N_age
+dim(n_ER) <- N_age
+dim(n_EI) <- N_age
+dim(n_Eage) <- N_age
+dim(n_Edead) <- N_age
 dim(n_Infec) <- N_age
 dim(n_IR) <- N_age
+dim(n_Iage) <- N_age
 dim(n_Id) <- N_age
 dim(n_Idead) <- N_age
 dim(n_Recov) <- N_age
 dim(n_RS) <- N_age
+dim(n_Rage) <- N_age
 dim(n_Rdead) <- N_age
 
 # 2. INITIAL VALUES ############################################################
 # Initial values (user-defined parameters)
 N_ini[] <- user()
 
-max_I_ini <- 0
-min_I_ini <- -10
+max_E_ini <- 0
+min_E_ini <- -10
+log_E_ini[] <- user()
+E_ini[] <- 10^(log_E_ini[i]*(max_E_ini-min_E_ini)+min_E_ini)*N_ini[i]
+# 
+# I_ini[] <- user()
+# E_ini[] <- (sigma_1/sigma_2)*I_ini[i]
 
-log_I_ini[] <- user()
-I_ini[] <- 10^(log_I_ini[i]*(max_I_ini-min_I_ini)+min_I_ini)*N_ini[i]
+# stratify log_delta
+log_delta1 <- user(0, min = -10, max = 1)
+log_delta2 <- user(0, min = -10, max = 1)
+
+delta[1] <- (10^(log_delta1))
+delta[2] <- (10^(log_delta2))
 
 # Age-structured states:
-initial(S[]) <- N_ini[i] -(I_ini[i]+0)
-initial(I[]) <- I_ini[i]
+initial(S[]) <- N_ini[i] -(E_ini[i]+0+0)
+initial(E[]) <- E_ini[i]
+initial(I[]) <- 0
 initial(R[]) <- 0
 
 # Initial states:
 initial(N_tot) <- sum(N_ini)
-initial(S_tot) <- sum(N_ini) -(sum(I_ini)+0)
-initial(I_tot) <- sum(I_ini)
+initial(S_tot) <- sum(N_ini) -(sum(E_ini)+0+0)
+initial(E_tot) <- sum(E_ini)
+initial(I_tot) <- 0
 initial(R_tot) <- 0
 
-initial(n_SI1_weekly) <- 0
-initial(n_SI2_weekly) <- 0
+initial(n_EI1_weekly) <- 0
+initial(n_EI2_weekly) <- 0
+
+initial(lambda[]) <- sum(foi_ij[i, ])
+initial(beta) <- beta_0 * (1 + beta_1 * cos(2*pi * time_shift_1))
 
 # 3. UPDATES ###################################################################
-N[] <- S[i] + I[i] + R[i]
+N[] <- S[i] + E[i] + I[i] + R[i]
 
 m[, ] <- user() # age-structured contact matrix
 
@@ -86,53 +133,102 @@ vacc_m[1, 2] <- 0
 vacc_m[2, 1] <- 0.9*0.862*theta # adult->child
 vacc_m[2, 2] <- 0
 
-beta <- beta_0 *(
-  (1+beta_1*cos(2*pi*((time_shift_1*(365))+time)/(365))))
+# beta <- beta_0 *(
+#   (1+beta_1*cos(2*pi*((time_shift_1*(365))+time)/(365))))
 
-foi_ij[, ] <- (if (time >= 2648)
-  beta * m[i, j] * (((I[j])/N[j]) * (1 - vacc_m[i, j]))
+foi_ij[, ] <- (if (time >= 2648*freq)
+  beta * m[i, j] * (((E[j] + I[j])/N[j]) * (1 - vacc_m[i, j]))
   else
-    beta * m[i, j] * (((I[j])/N[j]))
+    beta * m[i, j] * (((E[j] + I[j])/N[j]))
 )
 
-lambda[] <- sum(foi_ij[i, ])
+# lambda[] <- sum(foi_ij[i, ])
+
+# lambda[] <- (if (sum(foi_ij[i, ]) <= 0) 0 else
+#   (sum(foi_ij[i, ])))
 
 # Cumulative hazard
-p_Suscep[] <- lambda[i]+mu_0[i]
-p_Infec[] <- sigma_2+mu_0[i]+mu_1
-p_Recov[] <- wane+mu_0[i]
+p_Suscep[] <- lambda[i]+mu_0[i]+alpha[i]
+p_Exposd[] <- delta[i]+sigma_1+mu_0[i]+alpha[i]
+p_Infec[] <- sigma_2+mu_0[i]+mu_1+alpha[i]
+p_Recov[] <- wane+mu_0[i]+alpha[i]
+
+p_SE[] <- (if (lambda[i]/(lambda[i]+mu_0[i]+alpha[i]) <= 0) 0 else 
+  (lambda[i]/(lambda[i]+mu_0[i]+alpha[i])))
+p_Sage[] <- (if (alpha[i]/(mu_0[i]+alpha[i]) <= 0) 0 else 
+  (alpha[i]/(mu_0[i]+alpha[i])))
+
+p_EI[] <- (if (delta[i]/(delta[i]+sigma_1+mu_0[i]+alpha[i]) <= 0) 0 else 
+  (delta[i]/(delta[i]+sigma_1+mu_0[i]+alpha[i])))
+p_ER[] <- (if (sigma_1/(sigma_1+mu_0[i]+alpha[i]) <= 0) 0 else 
+  (sigma_1/(sigma_1+mu_0[i]+alpha[i])))
+p_Eage[] <- (if (alpha[i]/(mu_0[i]+alpha[i]) <= 0) 0 else
+  (alpha[i]/(mu_0[i]+alpha[i])))
+
+p_IR[] <- (if (sigma_2/(sigma_2+mu_0[i]+mu_1+alpha[i]) <= 0) 0 else
+  (sigma_2/(sigma_2+mu_0[i]+mu_1+alpha[i])))
+p_Id[] <- (if (mu_1/(mu_0[i]+mu_1+alpha[i]) <= 0) 0 else
+  (mu_1/(mu_0[i]+mu_1+alpha[i])))
+p_Iage[] <- (if (alpha[i]/(mu_0[i]+alpha[i]) <= 0) 0 else
+  (alpha[i]/(mu_0[i]+alpha[i])))
+
+p_RS[] <- (if (wane/(wane+mu_0[i]+alpha[i]) <= 0) 0 else
+  (wane/(wane+mu_0[i]+alpha[i])))
+p_Rage[] <- (if (alpha[i]/(mu_0[i]+alpha[i]) <= 0) 0 else
+  (alpha[i]/(mu_0[i]+alpha[i])))
+
 
 # Draws for numbers changing between compartments
 # Leaving S
 n_Suscep[] <- rbinom(S[i], 1- exp(-p_Suscep[i]*dt))
-n_SI[] <- rbinom(n_Suscep[i], lambda[i]/(lambda[i]+mu_0[i]))
+n_SE[] <- rbinom(n_Suscep[i], p_SE[i])
 # n_SR[] <- rbinom((n_Suscep[i] - n_SA[i]), vacc[i]/(lambda[i]+mu_0[i]))
-n_Sdead[] <- n_Suscep[i] - (n_SI[i])
+n_Sage[] <- rbinom((n_Suscep[i] - n_SE[i]), p_Sage[i])
+n_Sdead[] <- n_Suscep[i] - (n_SE[i]+n_Sage[i])
+
+# Leaving E
+n_Exposd[] <- rbinom(E[i], 1- exp(-p_Exposd[i]*dt))
+n_EI[] <- rbinom(n_Exposd[i], p_EI[i])
+n_ER[] <- rbinom((n_Exposd[i] - n_EI[i]), p_ER[i])
+n_Eage[] <- rbinom((n_Exposd[i] - n_EI[i] - n_ER[i]), p_Eage[i])
+n_Edead[] <- n_Exposd[i] - (n_EI[i]+n_ER[i]+n_Eage[i])
 
 # Leaving I
 n_Infec[] <- rbinom(I[i], 1- exp(-p_Infec[i]*dt))
-n_IR[] <- rbinom(n_Infec[i], sigma_2/(sigma_2+mu_0[i]+mu_1))
-n_Id[] <- rbinom((n_Infec[i] - n_IR[i]), mu_1/(mu_0[i]+mu_1))
-n_Idead[] <- n_Infec[i] - (n_IR[i])
+n_IR[] <- rbinom(n_Infec[i], p_IR[i])
+n_Id[] <- rbinom((n_Infec[i] - n_IR[i]), p_Id[i])
+n_Iage[] <- rbinom((n_Infec[i] - n_IR[i] - n_Id[i]), p_Iage[i])
+n_Idead[] <- n_Infec[i] - (n_IR[i]+n_Id[i]+n_Iage[i])
 
 # Leaving R
 n_Recov[] <- rbinom(R[i], 1- exp(-p_Recov[i]*dt))
-n_RS[] <- rbinom(n_Recov[i], wane/(wane+mu_0[i]))
-n_Rdead[] <- n_Recov[i] - n_RS[i]
+n_RS[] <- rbinom(n_Recov[i], p_RS[i])
+n_Rage[] <- rbinom((n_Recov[i] - n_RS[i]), p_Rage[i])
+n_Rdead[] <- n_Recov[i] - (n_RS[i]+n_Rage[i])
 
 # Equations for transitions between compartments by age group
-n_Sborn[] <- n_Sdead[i] + n_Idead[i] + n_Id[i] + n_Rdead[i]
-born <- sum(n_Sborn)
+n_Sborn[] <- n_Sdead[i] + n_Edead[i] + n_Idead[i] + n_Id[i] + n_Rdead[i]
+mu_birth <- 1/(70*365)
+born[1] <- rpois(mu_birth * sum(N) * dt)  # Poisson births
+born[2] <- n_Sage[1]+n_Eage[1]+n_Iage[1]+n_Rage[1]
+# born <- sum(n_Sborn)
 # born <- rpois(mu * sum(N) * dt)
 
-update(S[]) <- S[i] + (born*(i==1) + n_RS[i]) - (n_SI[i] + n_Sdead[i])
-update(I[]) <- I[i] + n_SI[i] - (n_IR[i] + n_Id[i] + n_Idead[i])
-update(R[]) <- R[i] + (n_IR[i]) - (n_RS[i] + n_Rdead[i])
+# update(S[]) <- S[i] + (born*(i==1) + n_RS[i]) - (n_SE[i] + n_Sdead[i])
+update(S[]) <- S[i] + (born[i] + n_RS[i]) - (n_SE[i] + n_Sdead[i])
+update(E[]) <- E[i] + (n_SE[i]) - (n_EI[i] + n_ER[i] + n_Edead[i])
+update(I[]) <- I[i] + (n_EI[i]) - (n_IR[i] + n_Id[i] + n_Idead[i])
+update(R[]) <- R[i] + (n_IR[i] + n_ER[i]) - (n_RS[i] + n_Rdead[i])
 
 # Core equations of the transitions
 update(N_tot) <- sum(N)
 update(S_tot) <- sum(S)
+update(E_tot) <- sum(E)
 update(I_tot) <- sum(I)
 update(R_tot) <- sum(R)
-update(n_SI1_weekly) <- if (step %% (7*freq) == 0) n_SI[1] else n_SI1_weekly + n_SI[1]
-update(n_SI2_weekly) <- if (step %% (7*freq) == 0) n_SI[2] else n_SI2_weekly + n_SI[2]
+update(n_EI1_weekly) <- if (step %% (7*freq) == 0) n_EI[1] else n_EI1_weekly + n_EI[1]
+update(n_EI2_weekly) <- if (step %% (7*freq) == 0) n_EI[2] else n_EI2_weekly + n_EI[2]
+
+update(lambda[]) <- sum(foi_ij[i, ])
+update(beta) <- beta_0*(
+  (1+beta_1*cos(2*pi*((time_shift_1*(365))+time)/(365))))
