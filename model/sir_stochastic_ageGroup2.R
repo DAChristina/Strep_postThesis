21d3a5cd3ea3be34e755b0f89651f9dfdacfa1bd
Nick Croucher <n.croucher@imperial.ac.uk>
2026-04-05 06:58:36 +0100

    Add age flows

--- model/sir_stochastic_ageGroup2.R
+++ model/sir_stochastic_ageGroup2.R
@@ -25,8 +25,15 @@ sigma_1 <- 1/hypo_sigma_1_day # test sigma_1 (A -> R) later
 # psi <- user(0, min = 0) # Immunity differences between children & adults
 sigma_2 <- 1 # Assumed acute phase, 1 day
 
-mu_0[1] <- 1/((80.70-14)*365)
-mu_0[2] <- 1/(14*365)
+# Ageing
+# group 1 = children, group 2 = adults
+age_rate[1] <- 1 / (14 * 365)
+age_rate[2] <- 0
+
+# Natural mortality
+# group 1 = children, group 2 = adults
+mu_0[1] <- 0
+mu_0[2] <- 1 / ((80.70 - 14) * 365)
 # mu_0[1] <- 0
 # mu_0[2] <- 0
 # mu <- 0
@@ -55,6 +62,7 @@ dim(vacc_m) <- c(N_age, N_age)
 dim(lambda) <- N_age
 dim(delta) <- N_age
 dim(mu_0) <- N_age
+dim(age_rate) <- N_age
 
 dim(p_Suscep) <- N_age
 dim(p_Asym) <- N_age
@@ -84,6 +92,10 @@ dim(n_Ddead) <- N_age
 dim(n_Resist) <- N_age
 dim(n_RS) <- N_age
 dim(n_Rdead) <- N_age
+dim(n_age_S) <- N_age
+dim(n_age_A) <- N_age
+dim(n_age_D) <- N_age
+dim(n_age_R) <- N_age
 
 # 2. INITIAL VALUES ############################################################
 # Initial values (user-defined parameters)
@@ -145,61 +157,75 @@ delta[2] <- (10^(log_delta1+rho))#*UK_calibration_adults
 # sigma_1[2] <- psi*hypo_sigma_1
 
 # Cumulative hazard
-p_Suscep[] <- lambda[i]+mu_0[i]
-p_Asym[] <- delta[i]+sigma_1+mu_0[i]
-p_Dis[] <- sigma_2+mu_1+mu_0[i]
-p_Rec[] <- wane+mu_0[i]
+p_Suscep[] <- lambda[i]+mu_0[i]+age_rate[i]
+p_Asym[] <- delta[i]+sigma_1+mu_0[i]+age_rate[i]
+p_Dis[] <- sigma_2+mu_1+mu_0[i]+age_rate[i]
+p_Rec[] <- wane+mu_0[i]+age_rate[i]
 
-p_SA[] <- (if (lambda[i]/(lambda[i]+mu_0[i]) <= 0) 0 else 
-  (lambda[i]/(lambda[i]+mu_0[i])))
+p_SA[] <- (if (lambda[i]/(lambda[i]+mu_0[i]+age_rate[i]) <= 0) 0 else 
+  (lambda[i]/(lambda[i]+mu_0[i]+age_rate[i])))
 
-p_AD[] <- (if (delta[i]/(delta[i]+sigma_1+mu_0[i]) <= 0) 0 else 
-  (delta[i]/(delta[i]+sigma_1+mu_0[i])))
-p_AR[] <- (if (sigma_1/(sigma_1+mu_0[i]) <= 0) 0 else 
-  (sigma_1/(sigma_1+mu_0[i])))
+p_AD[] <- (if (delta[i]/(delta[i]+sigma_1+mu_0[i]+age_rate[i]) <= 0) 0 else 
+  (delta[i]/(delta[i]+sigma_1+mu_0[i]+age_rate[i])))
+p_AR[] <- (if (sigma_1/(sigma_1+mu_0[i]+age_rate[i]) <= 0) 0 else 
+  (sigma_1/(sigma_1+mu_0[i]+age_rate[i])))
 
-p_DR[] <- (if (sigma_2/(sigma_2+mu_1+mu_0[i]) <= 0) 0 else 
-  (sigma_2/(sigma_2+mu_1+mu_0[i])))
-p_Dd[] <- (if (mu_1/(mu_1+mu_0[i]) <= 0) 0 else 
-  (mu_1/(mu_1+mu_0[i])))
+p_DR[] <- (if (sigma_2/(sigma_2+mu_1+mu_0[i]+age_rate[i]) <= 0) 0 else 
+  (sigma_2/(sigma_2+mu_1+mu_0[i]+age_rate[i])))
+p_Dd[] <- (if (mu_1/(mu_1+mu_0[i]+age_rate[i]) <= 0) 0 else 
+  (mu_1/(mu_1+mu_0[i]+age_rate[i])))
 
-p_RS[] <- (if (wane/(wane+mu_0[i]) <= 0) 0 else 
-  (wane/(wane+mu_0[i])))
+p_RS[] <- (if (wane/(wane+mu_0[i]+age_rate[i]) <= 0) 0 else 
+  (wane/(wane+mu_0[i]+age_rate[i])))
 
 
 # Draws for numbers changing between compartments
 # Leaving S
-n_Suscep[] <- rbinom(S[i], 1- exp(-p_Suscep[i]*dt))
+n_Suscep[] <- rbinom(S[i], 1 - exp(-p_Suscep[i]*dt))
 n_SA[] <- rbinom(n_Suscep[i], p_SA[i])
 # n_SR[] <- rbinom((n_Suscep[i] - n_SA[i]), vacc[i]/(lambda[i]+mu_0[i]))
-n_Sdead[] <- n_Suscep[i] - (n_SA[i])
+n_age_S[1] <- if (i == 1) rbinom(n_Suscep[i] - n_SA[i], age_rate[i]/(mu_0[i] + age_rate[i])) else 0
+n_age_S[2] <- 0
+n_Sdead[] <- n_Suscep[i] - n_SA[i] - n_age_S[i]
 
 # Leaving A
 n_Asym[] <- rbinom(A[i], 1- exp(-p_Asym[i]*dt))
 n_AD[] <- rbinom(n_Asym[i], p_AD[i])
 n_AR[] <- rbinom((n_Asym[i] - n_AD[i]), p_AR[i])
-n_Adead[] <- n_Asym[i] - (n_AD[i] + n_AR[i])
+n_age_A[1] <- if (i == 1) rbinom(n_Asym[i] - n_AD[i] - n_AR[i], age_rate[i]/(mu_0[i] + age_rate[i])) else 0
+n_age_A[2] <- 0
+n_Adead[] <- n_Asym[i] - n_AD[i] - n_AR[i] - n_age_A[i]
 
 # Leaving D
 n_Dis[] <- rbinom(D[i], 1- exp(-p_Dis[i]*dt))
 n_DR[] <- rbinom(n_Dis[i], p_DR[i])
 n_Dd[] <- rbinom((n_Dis[i] - n_DR[i]), p_Dd[i])
-n_Ddead[] <- n_Dis[i] - (n_DR[i] + n_Dd[i])
+n_age_D[1] <- if (i == 1) rbinom(n_Dis[i] - n_DR[i] - n_Dd[i], age_rate[i]/(mu_0[i] + age_rate[i])) else 0
+n_age_D[2] <- 0
+n_Ddead[] <- n_Dis[i] - n_DR[i] - n_Dd[i] - n_age_D[i]
 
 # Leaving R
 n_Resist[] <- rbinom(R[i], 1- exp(-p_Rec[i]*dt)) # RS is considered 0 in both age groups
 n_RS[] <- rbinom(n_Resist[i], p_RS[i])
-n_Rdead[] <- n_Resist[i] - n_RS[i]
+n_age_R[1] <- if (i == 1) rbinom(n_Resist[i] - n_RS[i], age_rate[i] / (mu_0[i] + age_rate[i])) else 0
+n_age_R[2] <- 0
+n_Rdead[] <- n_Resist[i] - n_RS[i] - n_age_R[i]
 
 # Equations for transitions between compartments by age group
 n_Sborn[] <- n_Sdead[i] + n_Adead[i] + n_Dd[i] + n_Ddead[i] + n_Rdead[i]
 born <- sum(n_Sborn)
-# born <- rpois(mu * sum(N) * dt)
 
-update(S[]) <- S[i] + (born*(i==1) + n_RS[i]) - (n_SA[i] + n_Sdead[i])
-update(A[]) <- A[i] + n_SA[i] - (n_AD[i] + n_AR[i] + n_Adead[i])
-update(D[]) <- D[i] + n_AD[i] - (n_DR[i] + n_Dd[i] + n_Ddead[i])
-update(R[]) <- R[i] + (n_AR[i] + n_DR[i]) - (n_RS[i] + n_Rdead[i])
+update(S[1]) <- S[1] + (born + n_RS[1]) - (n_SA[1] + n_Sdead[1] + n_age_S[1])
+update(S[2]) <- S[2] + (n_age_S[1] + n_RS[2]) - (n_SA[2] + n_Sdead[2] + n_age_S[2])
+
+update(A[1]) <- A[1] + n_SA[1] - (n_AD[1] + n_AR[1] + n_Adead[1] + n_age_A[1])
+update(A[2]) <- A[2] + n_SA[2] + n_age_A[1] - (n_AD[2] + n_AR[2] + n_Adead[2])
+
+update(D[1]) <- D[1] + n_AD[1] - (n_DR[1] + n_Dd[1] + n_Ddead[1] + n_age_D[1])
+update(D[2]) <- D[2] + n_AD[2] + n_age_D[1] - (n_DR[2] + n_Dd[2] + n_Ddead[2])
+
+update(R[1]) <- R[1] + (n_AR[1] + n_DR[1]) - (n_RS[1] + n_Rdead[1] + n_age_R[1])
+update(R[2]) <- R[2] + n_AR[2] + n_DR[2] + n_age_R[1] - (n_RS[2] + n_Rdead[2])
 
 # Core equations of the transitions
 update(N_tot) <- sum(N)
